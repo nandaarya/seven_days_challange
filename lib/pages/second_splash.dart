57230c679e37f223e60f17cfb35b79dbddbc43ae
Nanda Arya Putra <nandaarya404@gmail.com>
2022-09-21 21:25:20 +0700

    Second Splash Done

--- lib/pages/second_splash.dart
+++ lib/pages/second_splash.dart
@@ -6,8 +6,34 @@ class SecondSplash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Image.asset('assets/images/background.png'),
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.only(left: 77.0, right: 77.0, top: 70.0),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/images/house_icon.png',
+                width: 51,
+                height: 51,
+              ),
+              Text(
+                  'HouseQu',
+                style: GoogleFonts.montserrat(
+                  color: Color(0xff000000),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32.3,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
