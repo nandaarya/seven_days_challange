7972d83faa2ff05e5f857d4acb2d7523b28d6852
Nanda Arya Putra <nandaarya404@gmail.com>
2022-09-21 21:28:45 +0700

    delete debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class SevenDays extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SecondSplash(),
     );
   }
