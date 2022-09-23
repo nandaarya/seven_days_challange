1eeb3586da471b3cff77af4060ab14c6066bc8ad
Nanda Arya Putra <nandaarya404@gmail.com>
2022-09-23 20:54:59 +0700

    add pading to column

--- lib/pages/second_started.dart
+++ lib/pages/second_started.dart
@@ -6,21 +6,24 @@ class SecondStarted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Health First.'),
-          Text('Exercise together with our best community fit in the world'),
-          Container(
-            height: 402,
-            color: Colors.green,
-          ),
-          Container(
-            height: 55,
-            color: Color(0xffAFEA0D),
-            child: Text('Shape My Body'),
-          ),
-          Text('Terms & Conditions'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 40, right: 40),
+        child: Column(
+          children: [
+            Text('Health First.'),
+            Text('Exercise together with our best community fit in the world'),
+            Container(
+              height: 402,
+              color: Colors.green,
+            ),
+            Container(
+              height: 55,
+              color: Color(0xffAFEA0D),
+              child: Text('Shape My Body'),
+            ),
+            Text('Terms & Conditions'),
+          ],
+        ),
       ),
     );
   }
