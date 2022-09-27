07256e17952f8570ca488c6ab014121bc31f3bd8
Nanda Arya Putra <nandaarya404@gmail.com>
2022-09-27 20:54:44 +0700

    initial commit

---
+++ lib/pages/first_sign_in.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FirstSignIn extends StatelessWidget {
+  const FirstSignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
