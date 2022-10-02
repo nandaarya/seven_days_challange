7b91dd859306cde9de4b82dc2be17267262b8ab2
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-02 22:00:05 +0700

    add form style

--- lib/pages/second_sign_in.dart
+++ lib/pages/second_sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SecondSignIn extends StatelessWidget {
   const SecondSignIn({Key? key}) : super(key: key);
@@ -9,7 +10,14 @@ class SecondSignIn extends StatelessWidget {
       body: Column(
         children: [
           Image.asset('assets/images/signin_2.png'),
-
+          Text('Email Address'),
+          TextFormField(
+            style: GoogleFonts.openSans(
+              color: Color(0xff17171A),
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ],
       ),
     );
