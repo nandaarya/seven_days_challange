582acf74696ec6126cc13374e7dc4edb3b2dc7a5
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-02 21:49:26 +0700

    initial second_sign_in.dart

---
+++ lib/pages/second_sign_in.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondSignIn extends StatelessWidget {
+  const SecondSignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
