06a1bea4389ac0cc071163ef5fc15702baa82f26
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-04 21:34:45 +0700

    wrap all widget to singlechildscrollview to avoid overflow when fill form

--- lib/pages/second_sign_in.dart
+++ lib/pages/second_sign_in.dart
@@ -8,65 +8,67 @@ class SecondSignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 28, right: 28,top: 64),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image.asset(
-                'assets/images/signin_2.png',
-                width: 245,
-                height: 279,
-              ),
-            ),
-            SizedBox(height: 53,),
-            Text(
-              'Email Address',
-              style: GoogleFonts.openSans(
-                color: Color(0xff17171A),
-              ),
-            ),
-            SizedBox(height: 6),
-            TextFormField(
-              style: GoogleFonts.openSans(
-                color: Color(0xff17171A),
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-              decoration: InputDecoration(
-                fillColor: Color(0xffF3F3F3),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(71),
-                  borderSide: BorderSide.none,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 28, right: 28,top: 64),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  'assets/images/signin_2.png',
+                  width: 245,
+                  height: 279,
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Password',
-              style: GoogleFonts.openSans(
-                color: Color(0xff17171A),
-              ),
-            ),
-            SizedBox(height: 6),
-            TextFormField(
-              style: GoogleFonts.openSans(
-                color: Color(0xff17171A),
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-              decoration: InputDecoration(
-                fillColor: Color(0xffF3F3F3),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(71),
-                  borderSide: BorderSide.none,
+              SizedBox(height: 53,),
+              Text(
+                'Email Address',
+                style: GoogleFonts.openSans(
+                  color: Color(0xff17171A),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 6),
+              TextFormField(
+                style: GoogleFonts.openSans(
+                  color: Color(0xff17171A),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+                decoration: InputDecoration(
+                  fillColor: Color(0xffF3F3F3),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(71),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Password',
+                style: GoogleFonts.openSans(
+                  color: Color(0xff17171A),
+                ),
+              ),
+              SizedBox(height: 6),
+              TextFormField(
+                style: GoogleFonts.openSans(
+                  color: Color(0xff17171A),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+                decoration: InputDecoration(
+                  fillColor: Color(0xffF3F3F3),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(71),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
