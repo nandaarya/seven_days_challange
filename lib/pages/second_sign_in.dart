7a0148bded1caac98fa2c8a3cce19dae5ac00798
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-05 22:24:29 +0700

    add text

--- lib/pages/second_sign_in.dart
+++ lib/pages/second_sign_in.dart
@@ -68,6 +68,7 @@ class SecondSignIn extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 50,),
+              Text('Log In'),
             ],
           ),
         ),
