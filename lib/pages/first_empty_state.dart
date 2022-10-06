2285125d59f1f36e49c7390b5ee984eda0580a3d
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-06 22:16:11 +0700

    add center

--- lib/pages/first_empty_state.dart
+++ lib/pages/first_empty_state.dart
@@ -9,31 +9,33 @@ class FirstEmptyState extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(top: 148, left: 66, right: 66),
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/bag.png',
-              width: 239,
-              height: 210,
-            ),
-            SizedBox(height: 100),
-            Text(
-              'Success Order',
-              style: GoogleFonts.poppins(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/bag.png',
+                width: 239,
+                height: 210,
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'We will delivery your package \nas soon as possible',
-              style: GoogleFonts.poppins(
-                fontSize: 16,
+              SizedBox(height: 100),
+              Text(
+                'Success Order',
+                style: GoogleFonts.poppins(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            SizedBox(height: 50),
-            Text('Done'),
-          ],
+              SizedBox(height: 16),
+              Text(
+                'We will delivery your package \nas soon as possible',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 50),
+              Text('Done'),
+            ],
+          ),
         ),
       ),
     );
