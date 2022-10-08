6dc1e0eec4b59b5f8d91a6476478c8446f3db0b3
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-08 20:42:10 +0700

    add text style

--- lib/pages/second_empty_state.dart
+++ lib/pages/second_empty_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SecondEmptyState extends StatelessWidget {
   const SecondEmptyState({Key? key}) : super(key: key);
@@ -9,7 +10,12 @@ class SecondEmptyState extends StatelessWidget {
       backgroundColor: Color(0xff1B1B33),
       body: Column(
         children: [
-          Text('Boost Profit!'),
+          Text('Boost Profit!',
+          style: GoogleFonts.poppins(
+            color: Color(0xffFFFFFF),
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),),
           Text('Our tools are helping business to grow much faster'),
         ],
       ),
