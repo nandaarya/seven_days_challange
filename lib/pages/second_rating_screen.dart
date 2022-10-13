6ea613b6b833f03de903ee73260154dc298f9ed9
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-13 19:39:57 +0700

    initial second rating screen

---
+++ lib/pages/second_rating_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondRatingScreen extends StatelessWidget {
+  const SecondRatingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
