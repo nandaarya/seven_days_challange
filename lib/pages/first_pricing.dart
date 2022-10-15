94d0cbf42d481d52672e803212be206b2298c279
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-15 21:11:59 +0700

    initial first pricing screen

---
+++ lib/pages/first_pricing.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FirstPricingScreen extends StatelessWidget {
+  const FirstPricingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset('assets/images/mahkota.png'),
+        ],
+      ),
+    );
+  }
+}
