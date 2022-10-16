2fc64e6294dd2a6b0f88dafbc11b5c8d7449c6fd
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-16 21:29:07 +0700

    add padding

--- lib/pages/first_pricing.dart
+++ lib/pages/first_pricing.dart
@@ -7,105 +7,108 @@ class FirstPricingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Image.asset('assets/images/mahkota.png', height: 100, width: 100),
-            SizedBox(height: 48),
-            Text('Which one you wish to Upgrade?', ),
-            SizedBox(height: 50),
-            Container(
-              width: 315,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(39),
-                border: Border.all(
-                  color: Color(0xffD9DEEA),
-                  width: 2,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: Column(
+            children: [
+              Image.asset('assets/images/mahkota.png', height: 100, width: 100),
+              SizedBox(height: 48),
+              Text('Which one you wish to Upgrade?', ),
+              SizedBox(height: 50),
+              Container(
+                width: 315,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(39),
+                  border: Border.all(
+                    color: Color(0xffD9DEEA),
+                    width: 2,
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 17, right: 17,),
-                child: Row(
-                  children: [
-                    Image.asset('assets/images/celengan.png', width: 66, height: 61,),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 7, right: 7),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Money Security'),
-                          Text('support 24/7')
-                        ],
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 17, right: 17,),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/celengan.png', width: 66, height: 61,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 7, right: 7),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Money Security'),
+                            Text('support 24/7')
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 24),
-            Container(
-              width: 315,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(39),
-                border: Border.all(
-                  color: Color(0xffD9DEEA),
-                  width: 2,
+              SizedBox(height: 24),
+              Container(
+                width: 315,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(39),
+                  border: Border.all(
+                    color: Color(0xffD9DEEA),
+                    width: 2,
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 17, right: 17,),
-                child: Row(
-                  children: [
-                    Image.asset('assets/images/pricing_2list.png', width: 66, height: 61,),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 7, right: 7),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Automation'),
-                          Text('we provide Invoice'),
-                        ],
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 17, right: 17,),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/pricing_2list.png', width: 66, height: 61,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 7, right: 7),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Automation'),
+                            Text('we provide Invoice'),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 24),
-            Container(
-              width: 315,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(39),
-                border: Border.all(
-                  color: Color(0xffD9DEEA),
-                  width: 2,
+              SizedBox(height: 24),
+              Container(
+                width: 315,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(39),
+                  border: Border.all(
+                    color: Color(0xffD9DEEA),
+                    width: 2,
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 17, right: 17,),
-                child: Row(
-                  children: [
-                    Image.asset('assets/images/pricing_3list.png', width: 66, height: 61,),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 7, right: 7),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Balance Report'),
-                          Text('can up to 10k')
-                        ],
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 17, right: 17,),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/pricing_3list.png', width: 66, height: 61,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 7, right: 7),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Balance Report'),
+                            Text('can up to 10k')
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
