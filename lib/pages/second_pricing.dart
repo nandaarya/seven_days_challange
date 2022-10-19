3c0dabcc7a81ba6c68bab32646164cd2020e7ba9
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-19 22:24:50 +0700

    add space and button

--- lib/pages/second_pricing.dart
+++ lib/pages/second_pricing.dart
@@ -11,38 +11,79 @@ class SecondPricingScreen extends StatelessWidget {
         child: Column(
           children: [
             Image.asset('assets/images/pricing2.png', width: 164, height: 164),
+            SizedBox(height: 24),
             Text('Pro Features'),
+            SizedBox(height: 10),
             Text('Unlock the winner modules \nand get more insights'),
+            SizedBox(height: 50),
             Column(
               children: [
                 Row(
                   children: [
-                    Image.asset('assets/images/ic_check_pricing2.png', width: 24, height: 24,),
+                    Image.asset(
+                      'assets/images/ic_check_pricing2.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(width: 12),
                     Text('Unlock Our Top Charts'),
                   ],
                 ),
                 SizedBox(height: 26),
                 Row(
                   children: [
-                    Image.asset('assets/images/ic_check_pricing2.png', width: 24, height: 24,),
+                    Image.asset(
+                      'assets/images/ic_check_pricing2.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(width: 12),
                     Text('Save More than 1,000 Docs'),
                   ],
                 ),
                 SizedBox(height: 26),
                 Row(
                   children: [
-                    Image.asset('assets/images/ic_check_pricing2.png', width: 24, height: 24,),
+                    Image.asset(
+                      'assets/images/ic_check_pricing2.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(width: 12),
                     Text('24/7 Customer Support'),
                   ],
                 ),
                 SizedBox(height: 26),
                 Row(
                   children: [
-                    Image.asset('assets/images/ic_check_pricing2.png', width: 24, height: 24,),
+                    Image.asset(
+                      'assets/images/ic_check_pricing2.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(width: 12),
                     Text('Track Company’s Spending'),
                   ],
                 ),
               ],
+            ),
+            SizedBox(height: 58),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffE57C73),
+                borderRadius: BorderRadius.circular(31),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 7, right: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('Subscribe Now')),
+                    SizedBox(width: 40),
+                    Image.asset('assets/images/btn_arrow_pricing2.png', width: 41, height: 41,),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
