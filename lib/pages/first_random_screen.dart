663a7422d4bedb9677f5eddf3ac514fec5708271
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-23 22:20:12 +0700

    initial commit

---
+++ lib/pages/first_random_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FirstRandomScreen extends StatelessWidget {
+  const FirstRandomScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30, top: 36,),
+        child: Column(
+          children: [
+            Text('My Shopping Cart'),
+          ],
+        ),
+      ),
+    );
+  }
+}
