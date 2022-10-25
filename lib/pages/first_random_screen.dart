091d0ece9aca586b50cf885ece0eaae8e580103a
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-25 20:56:51 +0700

    add Item

--- lib/pages/first_random_screen.dart
+++ lib/pages/first_random_screen.dart
@@ -6,12 +6,40 @@ class FirstRandomScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffE5E5E5),
       body: Padding(
-        padding: const EdgeInsets.only(left: 30, right: 30, top: 36,),
+        padding: const EdgeInsets.only(
+          left: 30,
+          right: 30,
+          top: 36,
+        ),
         child: Column(
           children: [
             Text('My Shopping Cart'),
-            Text('Burger Malleta'),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset('assets/images/burger.png', width: 80, height: 80,),
+                    Column(children: [
+                      Text('Burger Malleta'),
+                      Text('McTheone'),
+                    ],)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(children: [
+                      Image.asset('assets/images/icon_random1.png', width: 22, height: 22,),
+                      Text('2'),
+                      Image.asset('assets/images/icon_random2.png', width: 22, height: 22,),
+                    ],),
+                    Text('\$90.00'),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
