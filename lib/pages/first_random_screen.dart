7e6947bda961a5595bd9bcf7c465b567cdb407dd
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-26 21:00:38 +0700

    duplicate item

--- lib/pages/first_random_screen.dart
+++ lib/pages/first_random_screen.dart
@@ -57,6 +57,45 @@ class FirstRandomScreen extends StatelessWidget {
                 ),
               ),
             ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xffFFFFFF),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset('assets/images/burger.png', width: 80, height: 80),
+                        SizedBox(width: 16),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Burger Malleta', style: itemTitleTextStyle,),
+                            Text('McTheone', style: detailItemTextStyle,),
+                          ],)
+                      ],
+                    ),
+                    SizedBox(height: 13),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(children: [
+                          Image.asset('assets/images/icon_random1.png', width: 22, height: 22,),
+                          SizedBox(width: 8),
+                          Text('2', style: priceTextStyle,),
+                          SizedBox(width: 8),
+                          Image.asset('assets/images/icon_random2.png', width: 22, height: 22,),
+                        ],),
+                        Text('\$90.00', style: priceTextStyle,),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
