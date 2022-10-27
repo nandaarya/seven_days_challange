9d15919c68ec05d781f2f11ab2cc3a19e3aa4b3b
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-27 21:59:06 +0700

    add decoration

--- lib/pages/first_random_screen.dart
+++ lib/pages/first_random_screen.dart
@@ -16,7 +16,10 @@ class FirstRandomScreen extends StatelessWidget {
         ),
         child: Column(
           children: [
-            Text('My Shopping Cart', style: title3TextStyle,),
+            Text(
+              'My Shopping Cart',
+              style: title3TextStyle,
+            ),
             SizedBox(height: 30),
             Container(
               decoration: BoxDecoration(
@@ -29,28 +32,52 @@ class FirstRandomScreen extends StatelessWidget {
                   children: [
                     Row(
                       children: [
-                        Image.asset('assets/images/burger.png', width: 80, height: 80),
+                        Image.asset('assets/images/burger.png',
+                            width: 80, height: 80),
                         SizedBox(width: 16),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                          Text('Burger Malleta', style: itemTitleTextStyle,),
-                          Text('McTheone', style: detailItemTextStyle,),
-                        ],)
+                            Text(
+                              'Burger Malleta',
+                              style: itemTitleTextStyle,
+                            ),
+                            Text(
+                              'McTheone',
+                              style: detailItemTextStyle,
+                            ),
+                          ],
+                        )
                       ],
                     ),
                     SizedBox(height: 13),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(children: [
-                          Image.asset('assets/images/icon_random1.png', width: 22, height: 22,),
-                          SizedBox(width: 8),
-                          Text('2', style: priceTextStyle,),
-                          SizedBox(width: 8),
-                          Image.asset('assets/images/icon_random2.png', width: 22, height: 22,),
-                        ],),
-                        Text('\$90.00', style: priceTextStyle,),
+                        Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/icon_random1.png',
+                              width: 22,
+                              height: 22,
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              '2',
+                              style: priceTextStyle,
+                            ),
+                            SizedBox(width: 8),
+                            Image.asset(
+                              'assets/images/icon_random2.png',
+                              width: 22,
+                              height: 22,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          '\$90.00',
+                          style: priceTextStyle,
+                        ),
                       ],
                     ),
                   ],
@@ -69,28 +96,52 @@ class FirstRandomScreen extends StatelessWidget {
                   children: [
                     Row(
                       children: [
-                        Image.asset('assets/images/mojito.png', width: 80, height: 80),
+                        Image.asset('assets/images/mojito.png',
+                            width: 80, height: 80),
                         SizedBox(width: 16),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('Mojito Orange', style: itemTitleTextStyle,),
-                            Text('The Bar', style: detailItemTextStyle,),
-                          ],)
+                            Text(
+                              'Mojito Orange',
+                              style: itemTitleTextStyle,
+                            ),
+                            Text(
+                              'The Bar',
+                              style: detailItemTextStyle,
+                            ),
+                          ],
+                        )
                       ],
                     ),
                     SizedBox(height: 13),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(children: [
-                          Image.asset('assets/images/icon_random1.png', width: 22, height: 22,),
-                          SizedBox(width: 8),
-                          Text('5', style: priceTextStyle,),
-                          SizedBox(width: 8),
-                          Image.asset('assets/images/icon_random2.png', width: 22, height: 22,),
-                        ],),
-                        Text('\$510.00', style: priceTextStyle,),
+                        Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/icon_random1.png',
+                              width: 22,
+                              height: 22,
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              '5',
+                              style: priceTextStyle,
+                            ),
+                            SizedBox(width: 8),
+                            Image.asset(
+                              'assets/images/icon_random2.png',
+                              width: 22,
+                              height: 22,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          '\$510.00',
+                          style: priceTextStyle,
+                        ),
                       ],
                     ),
                   ],
@@ -98,13 +149,28 @@ class FirstRandomScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 26),
-            Column(
-              children: [
-                Text('Informations'),
-                Row(children: [Text('Sub total'), Text('\$600.00')],),
-                Row(children: [Text('Delivery'), Text('\$80.00')],),
-                Row(children: [Text('Total'), Text('\$680.00')],),
-              ],
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xffFFFFFF),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Text('Informations'),
+                    Row(
+                      children: [Text('Sub total'), Text('\$600.00')],
+                    ),
+                    Row(
+                      children: [Text('Delivery'), Text('\$80.00')],
+                    ),
+                    Row(
+                      children: [Text('Total'), Text('\$680.00')],
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
         ),
