24592bc261204a3ffb705628d59d0147d8b7c3f3
Nanda Arya Putra <nandaarya404@gmail.com>
2022-10-30 22:50:16 +0700

    initial commit

---
+++ lib/pages/second_random_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecondRandomScreen extends StatelessWidget {
+  const SecondRandomScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
