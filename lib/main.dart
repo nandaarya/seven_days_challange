f96963c217b089bc80f2a185a48c552af50f5e53
Nanda Arya Putra <nandaarya404@gmail.com>
2023-01-16 22:27:25 +0700

    fix some warning

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:seven_days_challange/pages/first_empty_state.dart';
-import 'package:seven_days_challange/pages/first_pricing.dart';
-import 'package:seven_days_challange/pages/first_random_screen.dart';
-import 'package:seven_days_challange/pages/first_rating_screen.dart';
-import 'package:seven_days_challange/pages/first_sign_in.dart';
-import 'package:seven_days_challange/pages/first_splash.dart';
-import 'package:seven_days_challange/pages/first_started.dart';
-import 'package:seven_days_challange/pages/second_empty_state.dart';
-import 'package:seven_days_challange/pages/second_pricing.dart';
 import 'package:seven_days_challange/pages/second_random_screen.dart';
-import 'package:seven_days_challange/pages/second_rating_screen.dart';
-import 'package:seven_days_challange/pages/second_sign_in.dart';
-import 'package:seven_days_challange/pages/second_splash.dart';
-import 'package:seven_days_challange/pages/second_started.dart';
 
-void main() => runApp(SevenDays());
+void main() => runApp(const SevenDays());
 
 class SevenDays extends StatelessWidget {
   const SevenDays({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SecondRandomScreen(),
     );
