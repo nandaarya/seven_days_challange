dc5fae7294bb55f4a9ab17f19adb4d6443da184f
Nanda Arya Putra <nandaarya404@gmail.com>
2023-01-17 22:16:07 +0700

    fix some warning

--- lib/pages/first_started.dart
+++ lib/pages/first_started.dart
@@ -10,52 +10,50 @@ class FirstStarted extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               image: DecorationImage(
                   image: AssetImage('assets/images/background_started.png'),
                   fit: BoxFit.cover),
             ),
           ),
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 527.0),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 74.0, right: 74.0),
-                    child: Text(
-                      'Maximize Revenue',
-                      style: GoogleFonts.poppins(
-                        color: Color(0xffFFFFFF),
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
+          Padding(
+            padding: const EdgeInsets.only(top: 527.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 74.0, right: 74.0),
+                  child: Text(
+                    'Maximize Revenue',
+                    style: GoogleFonts.poppins(
+                      color: const Color(0xffFFFFFF),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                  SizedBox(height: 16),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 39.0, right: 36.0),
-                    child: Text(
-                      'Gain more profit from cryptocurrency without any risk involved',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                        color: Color(0xffFFFFFF),
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
-                      ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.only(left: 39.0, right: 36.0),
+                  child: Text(
+                    'Gain more profit from cryptocurrency without any risk involved',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      color: const Color(0xffFFFFFF),
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
                     ),
                   ),
-                  SizedBox(height: 40),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 148.0, right: 147.0),
-                    child: Image.asset(
-                      'assets/images/purple_icon.png',
-                      width: 80,
-                      height: 80,
-                    ),
+                ),
+                const SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.only(left: 148.0, right: 147.0),
+                  child: Image.asset(
+                    'assets/images/purple_icon.png',
+                    width: 80,
+                    height: 80,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
