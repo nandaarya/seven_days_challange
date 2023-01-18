0b4a9f6d967ee2b9352c46fbc4b917f65c5e70d5
Nanda Arya Putra <nandaarya404@gmail.com>
2023-01-18 22:49:03 +0700

    fix some warning

--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 // First Rating Screen
-Color whiteColor = Color(0xffFFFFFF);
+Color whiteColor = const Color(0xffFFFFFF);
 
 TextStyle foodTextStyle = GoogleFonts.poppins(
   color: whiteColor,
@@ -30,137 +30,137 @@ TextStyle rateTextStyle = GoogleFonts.poppins(
 // Second Rating Screen
 
 TextStyle headTextStyle = GoogleFonts.poppins(
-  color: Color(0xff121622),
+  color: const Color(0xff121622),
   fontSize: 20,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle subTextStyle = GoogleFonts.poppins(
-  color: Color(0xff808EAB),
+  color: const Color(0xff808EAB),
   fontSize: 16,
 );
 
 TextStyle messageTextStyle = GoogleFonts.poppins(
-  color: Color(0xff808EAB),
+  color: const Color(0xff808EAB),
   fontSize: 14,
 );
 
 TextStyle submitTextStyle = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontSize: 16,
   fontWeight: FontWeight.w600,
 );
 
 // First Pricing Screen
 TextStyle titleTextStyle = GoogleFonts.poppins(
-  color: Color(0xff191919),
+  color: const Color(0xff191919),
   fontSize: 22,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle itemTextStyle = GoogleFonts.poppins(
-  color: Color(0xff191919),
+  color: const Color(0xff191919),
   fontSize: 16,
   fontWeight: FontWeight.w500,
 );
 
 TextStyle detailTextStyle = GoogleFonts.poppins(
-  color: Color(0xffA3A8B8),
+  color: const Color(0xffA3A8B8),
   fontSize: 14,
   fontWeight: FontWeight.w300,
 );
 
 TextStyle upgradeTextStyle = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontSize: 18,
   fontWeight: FontWeight.w600,
 );
 
 // Second Pricing Screen
 TextStyle title2TextStyle = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontSize: 22,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle detail2TextStyle = GoogleFonts.poppins(
-  color: Color(0xff7F7FAD),
+  color: const Color(0xff7F7FAD),
   fontSize: 16,
 );
 
 TextStyle item2TextStyle = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontSize: 16,
 );
 
 TextStyle subscribeTextStyle = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontSize: 16,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle contactTextStyle = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontSize: 16,
 );
 
 // First Random Screen
 TextStyle title3TextStyle = GoogleFonts.poppins(
-  color: Color(0xff191919),
+  color: const Color(0xff191919),
   fontSize: 22,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle itemTitleTextStyle = GoogleFonts.poppins(
-  color: Color(0xff191919),
+  color: const Color(0xff191919),
   fontSize: 18,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle detailItemTextStyle = GoogleFonts.poppins(
-  color: Color(0xffA3A8B8),
+  color: const Color(0xffA3A8B8),
   fontSize: 14,
   fontWeight: FontWeight.w300,
 );
 
 TextStyle priceTextStyle = GoogleFonts.poppins(
-  color: Color(0xff191919),
+  color: const Color(0xff191919),
   fontSize: 16,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle infoTextStyle = GoogleFonts.poppins(
-  color: Color(0xff191919),
+  color: const Color(0xff191919),
   fontSize: 16,
 );
 
 TextStyle checkoutTitleTextStyle = GoogleFonts.poppins(
-  color: Color(0xff2E221B),
+  color: const Color(0xff2E221B),
   fontSize: 18,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle wishlistTitleTextStyle = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontSize: 18,
   fontWeight: FontWeight.w600,
 );
 
 // Second Random Screen
 TextStyle secondTitle = GoogleFonts.poppins(
-  color: Color(0xff000000),
+  color: const Color(0xff000000),
   fontSize: 26,
   fontWeight: FontWeight.w600,
 );
 
 TextStyle subsecondTitle = GoogleFonts.poppins(
-  color: Color(0xff000000),
+  color: const Color(0xff000000),
   fontWeight: FontWeight.w300,
   fontSize: 16,
 );
 
 TextStyle aboutsecondTitle = GoogleFonts.poppins(
-  color: Color(0xff000000),
+  color: const Color(0xff000000),
   fontWeight: FontWeight.w400,
   fontSize: 16,
 );
